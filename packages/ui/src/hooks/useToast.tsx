61a7ac684e4323ddb229a5510388f9cab4719a3c
Shim MunSeong <Puterism.K@gmail.com>
2024-01-27 23:04:50 +0900

    refactor: useToast에서 상수 객체로부터 toast 함수를 생성하는 구조로 변경

--- packages/ui/src/hooks/useToast.tsx
+++ packages/ui/src/hooks/useToast.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from '@emotion/react';
-import { useCallback } from 'react';
+import { useMemo } from 'react';
 import { ErrorIcon, toast } from 'react-hot-toast';
 import { InfoIcon, SuccessIcon, WarningIcon } from '../components/Toast/icons';
 
@@ -7,89 +7,70 @@ interface ToastOptions {
   duration?: number;
 }
 
+type ToastFunction = (message: string, options?: ToastOptions) => ReturnType<typeof toast>;
+
 const DEFAULT_DURATION = 3000;
 
 const useToast = () => {
   const theme = useTheme();
 
-  const success = useCallback(
-    (message: string, options?: ToastOptions) => {
-      const duration = options?.duration ?? DEFAULT_DURATION;
-
-      return toast(message, {
-        icon: <SuccessIcon />,
-        style: {
-          border: '1px solid #C3ECC2',
-          backgroundColor: 'rgba(239, 250, 239, 0.95)',
-          boxShadow: '0px 4px 15px 0px rgba(0, 0, 0, 0.15)',
-          color: theme.palette.grey.g90,
+  const toastVariables = useMemo(
+    () =>
+      ({
+        success: {
+          icon: <SuccessIcon />,
+          style: {
+            border: '1px solid #C3ECC2',
+            backgroundColor: 'rgba(239, 250, 239, 0.95)',
+            boxShadow: '0px 4px 15px 0px rgba(0, 0, 0, 0.15)',
+            color: theme.palette.grey.g90,
+          },
         },
-        duration,
-      });
-    },
-    [theme],
+        warning: {
+          icon: <WarningIcon />,
+          style: {
+            border: '1px solid #FCEACE',
+            backgroundColor: 'rgba(254, 250, 243, 0.95)',
+            boxShadow: '0px 4px 15px 0px rgba(0, 0, 0, 0.15)',
+            color: theme.palette.grey.g90,
+          },
+        },
+        error: {
+          icon: <ErrorIcon />,
+          style: {
+            border: '1px solid #FACBCF',
+            backgroundColor: 'rgba(254, 248, 248, 0.95)',
+            boxShadow: '0px 4px 15px 0px rgba(0, 0, 0, 0.15)',
+            color: theme.palette.grey.g90,
+          },
+        },
+        info: {
+          icon: <InfoIcon />,
+          style: {
+            border: '1px solid #C6E0FF',
+            background: 'rgba(239, 245, 255, 0.95)',
+            boxShadow: '0px 4px 15px 0px rgba(0, 0, 0, 0.15)',
+            color: theme.palette.grey.g90,
+          },
+        },
+      }) as const,
+    [theme.palette.grey.g90],
   );
 
-  const warning = useCallback(
-    (message: string, options?: ToastOptions) => {
-      const duration = options?.duration ?? DEFAULT_DURATION;
+  return Object.entries(toastVariables).reduce(
+    (acc: Record<string, ToastFunction>, [key, variables]) => ({
+      ...acc,
+      [key]: (message: string, options?: ToastOptions) => {
+        const duration = options?.duration ?? DEFAULT_DURATION;
 
-      return toast(message, {
-        icon: <WarningIcon />,
-        style: {
-          border: '1px solid #FCEACE',
-          backgroundColor: 'rgba(254, 250, 243, 0.95)',
-          boxShadow: '0px 4px 15px 0px rgba(0, 0, 0, 0.15)',
-          color: theme.palette.grey.g90,
-        },
-        duration,
-      });
-    },
-    [theme],
+        return toast(message, {
+          ...variables,
+          duration,
+        });
+      },
+    }),
+    {},
   );
-
-  const error = useCallback(
-    (message: string, options?: ToastOptions) => {
-      const duration = options?.duration ?? DEFAULT_DURATION;
-
-      return toast(message, {
-        icon: <ErrorIcon />,
-        style: {
-          border: '1px solid #FACBCF',
-          backgroundColor: 'rgba(254, 248, 248, 0.95)',
-          boxShadow: '0px 4px 15px 0px rgba(0, 0, 0, 0.15)',
-          color: theme.palette.grey.g90,
-        },
-        duration,
-      });
-    },
-    [theme],
-  );
-
-  const info = useCallback(
-    (message: string, options?: ToastOptions) => {
-      const duration = options?.duration ?? DEFAULT_DURATION;
-
-      return toast(message, {
-        icon: <InfoIcon />,
-        style: {
-          border: '1px solid #C6E0FF',
-          background: 'rgba(239, 245, 255, 0.95)',
-          boxShadow: '0px 4px 15px 0px rgba(0, 0, 0, 0.15)',
-          color: theme.palette.grey.g90,
-        },
-        duration,
-      });
-    },
-    [theme],
-  );
-
-  return {
-    success,
-    warning,
-    error,
-    info,
-  };
 };
 
 export default useToast;
