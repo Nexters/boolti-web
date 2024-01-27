48f05a118c6b3983b5b1547f76f98d3d637f3818
Minsu Kim <alstn2468_@naver.com>
2024-01-27 22:45:34 +0900

    fix: svg 경고 수정

--- apps/preview/src/App.tsx
+++ apps/preview/src/App.tsx
@@ -3,8 +3,8 @@ import { Button, TextButton, ThemeProvider } from '@boolti/ui';
 const Icon = () => (
   <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18ZM10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 13.866 13.866 17 10 17Z"
       fill="currentColor"
     />
