5c3938bec3c3babb201fa9e601f7a8346f1bbd24
Minsu Kim <alstn2468_@naver.com>
2024-01-27 20:41:47 +0900

    feat: ui 패키지에도 breakpoint 타입 추가

--- packages/ui/src/emotion.d.ts
+++ packages/ui/src/emotion.d.ts
@@ -2,10 +2,12 @@ import '@emotion/react';
 
 import palette from './systems/palette';
 import typo from './systems/typo';
+import breakpoint from './systems/breakpoint';
 
 declare module '@emotion/react' {
   export interface Theme {
     palette: typeof palette;
     typo: typeof typo;
+    breakpoint: typeof breakpoint;
   }
 }
