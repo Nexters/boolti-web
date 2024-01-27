e687f6cbd4bdaab8ce5bc8d6ff7ccced25aaa10b
Minsu Kim <alstn2468_@naver.com>
2024-01-27 13:57:04 +0900

    revert: jsxRuntime 제거, root에 preview 명령어추가

--- apps/preview/vite.config.ts
+++ apps/preview/vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: mode === 'production' ? 'classic' : 'automatic',
       jsxImportSource: '@emotion/react',
       babel: {
         plugins: ['@emotion/babel-plugin'],
       },
     }),
   ],
-}));
+});
