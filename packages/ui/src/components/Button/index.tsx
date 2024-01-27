e5fe32a111323cc6c0f52e6ee7edd240f38bb96e
Minsu Kim <alstn2468_@naver.com>
2024-01-27 21:03:36 +0900

    fix: 화살표 함수로 변경

--- packages/ui/src/components/Button/index.tsx
+++ packages/ui/src/components/Button/index.tsx
@@ -2,13 +2,13 @@ import Styled, { ButtonProps } from './Button.style';
 
 type Props = React.HTMLAttributes<HTMLButtonElement> & ButtonProps;
 
-function Button({ children, colorTheme, size, icon, ...rest }: Props) {
+const Button = ({ children, colorTheme, size, icon, ...rest }: Props) => {
   return (
     <Styled.Container colorTheme={colorTheme} size={size} {...rest}>
       {icon && <Styled.Icon>{icon}</Styled.Icon>}
       {children}
     </Styled.Container>
   );
-}
+};
 
 export default Button;
