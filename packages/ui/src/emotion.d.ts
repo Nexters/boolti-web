f4190dbeeadcd1796602f5107cccd5e4c4ca6aa9
Minsu Kim <alstn2468_@naver.com>
2024-01-27 16:14:45 +0900

    fix: 잘못 임포트한 것 수정

--- packages/ui/src/emotion.d.ts
+++ packages/ui/src/emotion.d.ts
@@ -1,7 +1,7 @@
 import '@emotion/react';
 
 import palette from './systems/palette';
-import typo from './systems.typo';
+import typo from './systems/typo';
 
 declare module '@emotion/react' {
   export interface Theme {
