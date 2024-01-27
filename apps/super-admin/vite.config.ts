89621ca256f7b1f922b9074fe86dcaf9eab96b9c
Minsu Kim <alstn2468_@naver.com>
2024-01-27 23:55:38 +0900

    chore: 서버 실행 포트 8080, 8081, 8082로 수정

--- apps/super-admin/vite.config.ts
+++ apps/super-admin/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8082,
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
