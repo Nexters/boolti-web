05515a51fbe075e702c910a052d845ff5f81758a
Minsu Kim <alstn2468_@naver.com>
2024-01-29 21:29:22 +0900

    feat: Icon 컴포넌트 추가 (#21)
    * feat: Icon 컴포넌트 추가
    
    * feat: 버튼에 들어간 아이콘 사이즈 수정
    
    * feat: Icon을 뒤에 붙이도록 수정
    
    * fix: 타입 오류 수정
    
    * feat: @boolti/icon 패키지 분리
    
    * fix: lint 오류 수정

---
+++ packages/icon/src/components/ChevronRight.tsx
@@ -0,0 +1,19 @@
+export const ChevronRight = ({ size }: { size: 20 | 24 }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9 18L15 12L9 6"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
