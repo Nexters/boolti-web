941553849041a02399f0ec08e3b237e9fc5d6437
Minsu Kim <alstn2468_@naver.com>
2024-02-03 14:58:46 +0900

    fix: port 설정 제거

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 8081,
-  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
