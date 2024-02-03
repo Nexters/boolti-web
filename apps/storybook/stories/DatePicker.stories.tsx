e68defe25436f696f00ab89a8730d29999802d59
Minsu Kim <alstn2468_@naver.com>
2024-02-03 14:50:46 +0900

    feat: TextField, DatePicker도 스토리 추가

---
+++ apps/storybook/stories/DatePicker.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { DatePicker } from '@boolti/ui';
+
+const meta: Meta<typeof DatePicker> = {
+  title: '컴포넌트/데이트피커',
+  component: DatePicker,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof DatePicker>;
+
+export const small: Story = {
+  args: { size: 'small' },
+};
+
+export const big: Story = {
+  args: { size: 'big' },
+};
---
+++ apps/storybook/stories/TextField.stories.tsx
@@ -0,0 +1,85 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { TextField } from '@boolti/ui';
+
+const meta: Meta<typeof TextField> = {
+  title: '컴포넌트/텍스트필드',
+  component: TextField,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TextField>;
+
+export const Text: Story = {
+  args: {
+    size: 'small',
+    value: 'Text 타입 인풋',
+    inputType: 'text',
+  },
+};
+
+export const Date: Story = {
+  args: {
+    size: 'small',
+    value: '2024-02-03',
+    inputType: 'date',
+  },
+};
+
+export const File: Story = {
+  args: {
+    size: 'small',
+    fileName: 'file.png',
+    inputType: 'file',
+  },
+};
+
+export const With_Button: Story = {
+  args: {
+    size: 'small',
+    value: 'Text 타입 인풋',
+    inputType: 'text',
+    buttonProps: {
+      children: 'BUTTON',
+    },
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    size: 'small',
+    inputType: 'text',
+    value: 'Text 타입 인풋',
+    disabled: true,
+  },
+};
+
+export const Disabled_With_Button: Story = {
+  args: {
+    size: 'small',
+    inputType: 'text',
+    value: 'Text 타입 인풋',
+    disabled: true,
+    buttonProps: {
+      children: 'BUTTON',
+    },
+  },
+};
+
+export const Placeholder: Story = {
+  args: {
+    size: 'small',
+    inputType: 'text',
+    placeholder: 'Placeholder',
+    disabled: true,
+  },
+};
+
+export const Big: Story = {
+  args: {
+    size: 'big',
+    value: 'Text 타입 인풋',
+    inputType: 'text',
+  },
+};
