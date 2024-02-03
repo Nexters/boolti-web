c913a51e097b203218456da92550338f5767a07c
Minsu Kim <alstn2468_@naver.com>
2024-02-04 01:04:00 +0900

    feat: index 기반으로 렌더링하도록 설정

--- apps/admin/src/components/SettlementDialogContent/index.tsx
+++ apps/admin/src/components/SettlementDialogContent/index.tsx
@@ -5,32 +5,36 @@ import { bankItems } from '~/constants/bankItems';
 import Styled from './SettlementDialogContent.styles';
 import { useState } from 'react';
 
+const titles = [
+  '은행을 선택해 주세요.',
+  '계좌번호와 계좌주를 입력해 주세요.',
+  '입력하신 계좌 정보를 다시 한 번 확인해주세요.',
+];
+
 const SettlementDialogContent = () => {
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
   const [selectedBank, setSelectedBank] = useState<string | null>(null);
   return (
     <Styled.Container>
-      {
-        <Styled.Title>
-          {!selectedBank ? '은행을 선택해 주세요.' : '계좌번호와 계좌주를 입력해 주세요.'}
-        </Styled.Title>
-      }
-      <Styled.BankList>
-        {bankItems.map((bankItem) => (
-          <Styled.BankItem key={bankItem.name}>
-            <Styled.BankItemButton
-              isNull={selectedBank === null}
-              isSelected={selectedBank === bankItem.name}
-              onClick={() => {
-                setSelectedBank(bankItem.name);
-              }}
-            >
-              <Styled.BankIcon>{<bankItem.icon />}</Styled.BankIcon>
-              <Styled.BankName>{bankItem.name}</Styled.BankName>
-            </Styled.BankItemButton>
-          </Styled.BankItem>
-        ))}
-      </Styled.BankList>
+      <Styled.Title>{titles[currentStepIndex]}</Styled.Title>
+      {currentStepIndex === 0 && (
+        <Styled.BankList>
+          {bankItems.map((bankItem) => (
+            <Styled.BankItem key={bankItem.name}>
+              <Styled.BankItemButton
+                isNull={selectedBank === null}
+                isSelected={selectedBank === bankItem.name}
+                onClick={() => {
+                  setSelectedBank(bankItem.name);
+                }}
+              >
+                <Styled.BankIcon>{<bankItem.icon />}</Styled.BankIcon>
+                <Styled.BankName>{bankItem.name}</Styled.BankName>
+              </Styled.BankItemButton>
+            </Styled.BankItem>
+          ))}
+        </Styled.BankList>
+      )}
       <Styled.ButtonContainer>
         {currentStepIndex !== 0 && (
           <Button type="button" colorTheme="line" size="bold">
