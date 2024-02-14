fb410698e23df0d4acc73ef38c80e6fcf7382fd4
Minsu Kim <alstn2468_@naver.com>
2024-02-14 22:14:52 +0900

    fix: 병합 잘못된 것 수정

--- packages/api/src/queryKey.ts
+++ packages/api/src/queryKey.ts
@@ -47,23 +47,23 @@ export const showQueryKeys = createQueryKeys('show', {
         },
       });
     },
-    salesTicketList: (showId: number) => ({
-      queryKey: [showId],
-      queryFn: () =>
-        fetcher.get<ShowSalesTicketResponse>(`web/v1/host/shows/${showId}/sales-tickets`),
-    }),
-    invitationTicketList: (showId: number) => ({
-      queryKey: [showId],
-      queryFn: () =>
-        fetcher.get<ShowInvitationTicketResponse>(`web/v1/host/shows/${showId}/invitation-tickets`),
-    }),
-    invitationCodeList: (invitationTicketId: number) => ({
-      queryKey: [invitationTicketId],
-      queryFn: () =>
-        fetcher.get<ShowInvitationCodeListResponse>(
-          `web/v1/host/invitation-tickets/${invitationTicketId}/invitation-codes`,
-        ),
-    }),
+  }),
+  salesTicketList: (showId: number) => ({
+    queryKey: [showId],
+    queryFn: () =>
+      fetcher.get<ShowSalesTicketResponse>(`web/v1/host/shows/${showId}/sales-tickets`),
+  }),
+  invitationTicketList: (showId: number) => ({
+    queryKey: [showId],
+    queryFn: () =>
+      fetcher.get<ShowInvitationTicketResponse>(`web/v1/host/shows/${showId}/invitation-tickets`),
+  }),
+  invitationCodeList: (invitationTicketId: number) => ({
+    queryKey: [invitationTicketId],
+    queryFn: () =>
+      fetcher.get<ShowInvitationCodeListResponse>(
+        `web/v1/host/invitation-tickets/${invitationTicketId}/invitation-codes`,
+      ),
   }),
 });
 
