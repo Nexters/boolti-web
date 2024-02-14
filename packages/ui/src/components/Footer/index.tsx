a19221584a5ca6a97300700602fe0409203af5ef
Minsu Kim <alstn2468_@naver.com>
2024-02-14 23:45:38 +0900

    feat: 푸터 변경사항 반영 (#53)

--- packages/ui/src/components/Footer/index.tsx
+++ packages/ui/src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { GithubIcon, InstagramIcon, KakaotalkIcon } from '@boolti/icon';
+import { KakaotalkIcon } from '@boolti/icon';
 
 import Styled from './Footer.styles';
 
@@ -13,26 +13,42 @@ const Footer = () => {
           <Styled.Text>
             {'사업자 등록번호 : 202-43-63442 | 대표 : 김혜선' +
               '\n' +
-              '호스팅 서비스 : 스튜디오 불티 | 제2024-서울00-0000호(더미)' +
+              '호스팅 서비스 : 스튜디오 불티 | 통신판매업 신고번호 : 2024-화도수동-0518' +
               '\n' +
-              'TEL : 0507-0000-0000(더미) | 이메일 : studio.boolti@gmail.com'}
+              'TEL : 0507-1363-5690 | 이메일 : studio.boolti@gmail.com'}
           </Styled.Text>
           <Styled.LinkTextGroup>
             <Styled.Text>
-              <Styled.Link>개인정보 처리방침</Styled.Link>
+              <Styled.Link
+                href="https://boolti.notion.site/5f73661efdcd4507a1e5b6827aa0da70"
+                target="_blank"
+                rel="noreferrer noopener nofollow"
+              >
+                개인정보 처리방침
+              </Styled.Link>
               {` | `}
-              <Styled.Link>서비스 이용약관</Styled.Link>
+              <Styled.Link
+                href="https://boolti.notion.site/b4c5beac61c2480886da75a1f3afb982"
+                target="_blank"
+                rel="noreferrer noopener nofollow"
+              >
+                서비스 이용약관
+              </Styled.Link>
             </Styled.Text>
           </Styled.LinkTextGroup>
         </Styled.TextGroup>
         <Styled.IconGroup>
-          <Styled.IconLink>
+          {/* <Styled.IconLink>
             <InstagramIcon />
           </Styled.IconLink>
           <Styled.IconLink>
             <GithubIcon />
-          </Styled.IconLink>
-          <Styled.IconLink>
+          </Styled.IconLink> */}
+          <Styled.IconLink
+            href="http://pf.kakao.com/_pVxfxaG/chat"
+            target="_blank"
+            rel="noreferrer noopener nofollow"
+          >
             <KakaotalkIcon />
           </Styled.IconLink>
         </Styled.IconGroup>
