569a91adc34325e0980d1396412dda21f3441b0f
Minsu Kim <alstn2468_@naver.com>
2024-02-22 00:34:13 +0900

    fix: means 필드 이용하도록 수정

--- apps/admin/src/components/ReservationTable/index.tsx
+++ apps/admin/src/components/ReservationTable/index.tsx
@@ -37,9 +37,9 @@ const columns = [
     header: '결제 금액',
     cell: (props) => `${props.getValue().toLocaleString()}원`,
   }),
-  columnHelper.accessor('ticketType', {
+  columnHelper.accessor('means', {
     header: '결제 방법',
-    cell: (props) => (props.getValue() === 'INVITE' ? '초청 코드' : '카드 결제'),
+    cell: (props) => (props.getValue() === 'CARD' ? '카드 결제' : props.getValue() === 'ACCOUNT_TRANSFER' ? '계좌 이체' : ''),
   }),
   columnHelper.accessor('ticketIssuedAt', {
     header: '발권일시',
