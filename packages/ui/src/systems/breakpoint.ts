a0e37c21506af0dda74591a757c5dbedf465b21b
Minsu Kim <alstn2468_@naver.com>
2024-02-24 18:11:11 +0900

    feat: 미디어쿼리 유틸 추가

--- packages/ui/src/systems/breakpoint.ts
+++ packages/ui/src/systems/breakpoint.ts
@@ -1,5 +1,8 @@
 const breakpoint = {
   desktop: '1120px',
+  mobile: '640',
 } as const;
+
+export const mq = (key: keyof typeof breakpoint) => `@media (min-width: ${breakpoint[key]}px)`;
 
 export default breakpoint;
