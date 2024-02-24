df9dc7bab3995e11565f249d7b269321fbb08f00
Minsu Kim <alstn2468_@naver.com>
2024-02-25 01:24:24 +0900

    feat: 계좌 정보 입력 다이얼로그 모바일 대응 (#71)
    * feat: useDeviceWidth 추가 및 다이얼로그 스타일 개선
    
    * feat: 입장 확인 다이얼로그 모바일 대응
    
    * feat: 닫기 가능하도록 수정
    
    * feat: 계좌 정보 입력 다이얼로그 모바일 대응
    
    * chore: 린트 오류 수정
    
    * fix: 에러 있을 때 대응

--- packages/ui/src/components/TextField/index.tsx
+++ packages/ui/src/components/TextField/index.tsx
@@ -8,13 +8,23 @@ import Styled, { TextFieldProps } from './TextField.styles';
 type Props = Omit<React.ComponentProps<'input'>, 'size' | 'type'> & TextFieldProps;
 
 const TextField = forwardRef<HTMLInputElement, Props>(function TextField(
-  { disabled, size, inputType, buttonProps, placeholder, id, errorMessage, ...rest }: Props,
+  {
+    disabled,
+    size,
+    inputType,
+    buttonProps,
+    placeholder,
+    id,
+    errorMessage,
+    className,
+    ...rest
+  }: Props,
   ref,
 ) {
   const uuid = useRef<string>(id ?? nanoid(6));
 
   return (
-    <Styled.Container disabled={disabled} size={size} inputType={inputType}>
+    <Styled.Container disabled={disabled} size={size} className={className} inputType={inputType}>
       <Styled.InputContainer>
         {inputType === 'date' && (
           <Styled.InputLabel hasError={!!errorMessage} htmlFor={uuid.current} disabled={disabled}>
