6def1e5971fa13aa3679b44e84bfc08e6ebe24e1
Minsu Kim <alstn2468_@naver.com>
2024-03-17 21:39:46 +0900

    fix: 에러 일때 보더 색상 적용

--- packages/ui/src/components/TextField/TextField.styles.ts
+++ packages/ui/src/components/TextField/TextField.styles.ts
@@ -65,7 +65,8 @@ const Input = styled.input<{ hasError?: boolean }>`
   padding: 12px 13px;
   color: ${({ theme }) => theme.palette.grey.g90};
   border: 1px solid
-    ${({ hasError, theme }) => (hasError ? theme.palette.status.error : theme.palette.grey.g90)};
+    ${({ hasError, theme }) =>
+      hasError ? `${theme.palette.status.error} !important` : theme.palette.grey.g90};
   background: ${({ theme }) => theme.palette.grey.w};
   ${({ theme }) => theme.typo.b3};
   &:placeholder-shown {
