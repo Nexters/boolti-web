478bbd64d54f7c81dfc487aa2baef8491811fc9d
Minsu Kim <alstn2468_@naver.com>
2024-04-29 01:30:56 +0900

    feat: 필터링할때 쓸 드롭다운 메뉴 추가

--- apps/super-admin/src/pages/HomePage/index.tsx
+++ apps/super-admin/src/pages/HomePage/index.tsx
@@ -1,14 +1,15 @@
-import { LogoutOutlined } from '@ant-design/icons';
+import { DownOutlined, LogoutOutlined } from '@ant-design/icons';
 import { LOCAL_STORAGE, useAdminLogout } from '@boolti/api';
 import { BooltiSmallLogo } from '@boolti/icon';
-import { Card, Flex, Layout, Menu, Pagination, Typography } from 'antd';
+import { Button, Card, Dropdown, Flex, Layout, Menu, Pagination, Typography } from 'antd';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { PATH } from '~/constants/routes';
 
 const { Content, Header } = Layout;
 
-const items: React.ComponentProps<typeof Menu>['items'] = [
+const headerItems: React.ComponentProps<typeof Menu>['items'] = [
   {
     key: 'logout',
     icon: <LogoutOutlined />,
@@ -17,9 +18,20 @@ const items: React.ComponentProps<typeof Menu>['items'] = [
   },
 ];
 
+const selectItems = [
+  { key: '1', label: '전체' },
+  { key: '2', label: '판매 전' },
+  { key: '3', label: '판매 중' },
+  { key: '4', label: '판매 종료' },
+  { key: '5', label: '정산 필요' },
+  { key: '6', label: '정산 중' },
+  { key: '7', label: '정산 완료' },
+];
+
 const HomePage = () => {
   const { mutateAsync } = useAdminLogout();
   const navigate = useNavigate();
+  const [selectedItem, setSelectedItem] = useState('1');
   return (
     <Layout>
       <Header style={{ display: 'flex', alignItems: 'center' }}>
@@ -29,7 +41,7 @@ const HomePage = () => {
           selectable={false}
           theme="dark"
           mode="horizontal"
-          items={items}
+          items={headerItems}
           onClick={async (e) => {
             switch (e.key) {
               case 'logout': {
@@ -57,23 +69,45 @@ const HomePage = () => {
           }}
         >
           <Flex vertical>
-            <Typography.Title level={3} style={{ marginBottom: 32 }}>
+            <Typography.Title level={3} style={{ marginBottom: 36 }}>
               공연목록
             </Typography.Title>
+
+            <Dropdown
+              trigger={['click']}
+              menu={{
+                items: selectItems,
+                selectable: true,
+                defaultSelectedKeys: ['1'],
+                onClick: (e) => {
+                  setSelectedItem(e.key);
+                },
+              }}
+            >
+              <Button
+                style={{ marginBottom: 24, width: 130, textAlign: 'left', marginLeft: 'auto' }}
+              >
+                <Flex align="center" justify="space-between">
+                  {selectItems.find(({ key }) => key === selectedItem)?.label}
+                  <DownOutlined />
+                </Flex>
+              </Button>
+            </Dropdown>
+
             <Flex gap="small" wrap="wrap">
-              <Card title="Card title" style={{ width: 'calc(50% - 8px)' }}>
+              <Card title="Card title" style={{ width: 'calc(50% - 4px)' }}>
                 Card content
               </Card>
 
-              <Card title="Card title" style={{ width: 'calc(50% - 8px)' }}>
+              <Card title="Card title" style={{ width: 'calc(50% - 4px)' }}>
                 Card content
               </Card>
 
-              <Card title="Card title" style={{ width: 'calc(50% - 8px)' }}>
+              <Card title="Card title" style={{ width: 'calc(50% - 4px)' }}>
                 Card content
               </Card>
 
-              <Card title="Card title" style={{ width: 'calc(50% - 8px)' }}>
+              <Card title="Card title" style={{ width: 'calc(50% - 4px)' }}>
                 Card content
               </Card>
             </Flex>
