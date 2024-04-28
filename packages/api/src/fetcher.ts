8c16501c2beffd59822074d0b461ea76ea76e07d
Minsu Kim <alstn2468_@naver.com>
2024-04-28 23:37:25 +0900

    feat: 환경 변수 조건문 수정

--- packages/api/src/fetcher.ts
+++ packages/api/src/fetcher.ts
@@ -5,7 +5,7 @@ import BooltiHTTPError, { isBooltiHTTPError } from './BooltiHTTPError';
 import { LOCAL_STORAGE } from './constants';
 
 const API_URL = import.meta.env.VITE_BASE_API_URL;
-const IS_SUPER_ADMIN = import.meta.env.IS_SUPER_ADMIN ?? false;
+const IS_SUPER_ADMIN = import.meta.env.IS_SUPER_ADMIN === 'true';
 
 interface PostRefreshTokenResponse {
   accessToken: string;
