08299562745ee7a076199f207feb8fa87f1eb5d1
Minsu Kim <alstn2468_@naver.com>
2024-06-10 23:23:07 +0900

    feat: framer-motion 설치 및 LazyMotion 적용

--- apps/admin/src/pages/Landing/LandingPage.tsx
+++ apps/admin/src/pages/Landing/LandingPage.tsx
@@ -1,19 +1,22 @@
 import { BooltiDark } from '@boolti/icon';
+import { domAnimation, LazyMotion } from 'framer-motion';
 
 import Styled from './LandingPage.styles';
 
 const LandingPage = () => {
   return (
-    <Styled.Container>
-      <Styled.Header>
-        <Styled.HeaderContaienr>
-          <Styled.BooltiIcon>
-            <BooltiDark />
-          </Styled.BooltiIcon>
-        </Styled.HeaderContaienr>
-      </Styled.Header>
-      <Styled.KeyVisual />
-    </Styled.Container>
+    <LazyMotion features={domAnimation}>
+      <Styled.Container>
+        <Styled.Header>
+          <Styled.HeaderContaienr>
+            <Styled.BooltiIcon>
+              <BooltiDark />
+            </Styled.BooltiIcon>
+          </Styled.HeaderContaienr>
+        </Styled.Header>
+        <Styled.KeyVisual />
+      </Styled.Container>
+    </LazyMotion>
   );
 };
 
