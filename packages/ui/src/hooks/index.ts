8b2874abd8cca3163d6581cb27d73dc1436b1bf2
hexdrinker <hexdrinker2020@gmail.com>
2024-06-12 15:26:42 +0900

    feat: dropdown hook 추가

--- packages/ui/src/hooks/index.ts
+++ packages/ui/src/hooks/index.ts
@@ -1,5 +1,6 @@
 import useConfirm from './useConfirm';
 import useDialog from './useDialog';
+import useDropdown from './useDropdown';
 import useToast from './useToast';
 
-export { useConfirm, useDialog, useToast };
+export { useConfirm, useDialog, useDropdown, useToast };
---
+++ packages/ui/src/hooks/useDropdown.ts
@@ -0,0 +1,31 @@
+import { useEffect, useRef, useState } from 'react';
+
+const useDropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutSide);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutSide);
+    };
+  }, [isOpen]);
+
+  const toggleDropdown = () => {
+    setIsOpen((isOpen) => !isOpen);
+  };
+
+  const handleClickOutSide = (e: Event) => {
+    const current = dropdownRef.current;
+    if (isOpen && current && !current.contains(e.target as Node)) setIsOpen(false);
+  };
+
+  return {
+    isOpen,
+    dropdownRef,
+    toggleDropdown,
+  };
+};
+
+export default useDropdown;
