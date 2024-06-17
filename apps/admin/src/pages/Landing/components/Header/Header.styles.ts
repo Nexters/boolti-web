fb173cbc2548403fc7401754c02a588d33966841
hexdrinker <hexdrinker2020@gmail.com>
2024-06-18 00:33:09 +0900

    fix: 랜딩 페이지에서 로고 클릭 시 페이지 최상단으로 이동하는 동작 추가, 스타일 일부 수정

--- apps/admin/src/pages/Landing/components/Header/Header.styles.ts
+++ apps/admin/src/pages/Landing/components/Header/Header.styles.ts
@@ -35,6 +35,9 @@ const HeaderContaienr = styled(m.div)`
 const BooltiIcon = styled.div`
   width: 55.5px;
   height: 21px;
+  cursor: pointer;
+  z-index: 1;
+
   ${mq} {
     width: 74px;
     height: 28px;
