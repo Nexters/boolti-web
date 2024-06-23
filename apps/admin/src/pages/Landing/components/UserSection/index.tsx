e8bf165b8d2e71bb90fd703ec83a7a5528e71a3e
Park YuongHo <hexdrinker2020@gmail.com>
2024-06-23 19:35:45 +0900

    lint fix

--- apps/admin/src/pages/Landing/components/UserSection/index.tsx
+++ apps/admin/src/pages/Landing/components/UserSection/index.tsx
@@ -3,8 +3,8 @@ import { useNavigate } from 'react-router-dom';
 
 import entranceNotificationImg from '~/assets/images/entrance-notification.png';
 import mobileTicketPreivewImg from '~/assets/images/mobile-ticket-preview.png';
+import pcTicketPreviewImg from '~/assets/images/pc-ticket-preview.png';
 import showInfoImg from '~/assets/images/show-info.png';
-import ticketPreviewImg from '~/assets/images/pc-ticket-preview.png';
 import ticketPurchaseImg from '~/assets/images/ticket-purchase.png';
 import { LINK } from '~/constants/link';
 import { PATH } from '~/constants/routes';
@@ -39,7 +39,7 @@ const UserSection = () => {
         >
           앱 바로가기
         </Styled.Button>
-        <Styled.TicketPreviewImage src={ticketPreviewImg} />
+        <Styled.TicketPreviewImage src={pcTicketPreviewImg} />
         <Styled.MobileTicketPreviewImage src={mobileTicketPreivewImg} />
       </Styled.Container>
       <FeatureItem
