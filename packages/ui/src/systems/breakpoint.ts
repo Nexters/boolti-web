27e941f07936b93eecd0d1680f13169d3caee1ba
Park YuongHo <hexdrinker2020@gmail.com>
2024-06-25 23:39:48 +0900

    fix: 브레이크 포인트 테블릿 값 추가, 미디어쿼리명 변경

--- packages/ui/src/systems/breakpoint.ts
+++ packages/ui/src/systems/breakpoint.ts
@@ -1,8 +1,10 @@
 const breakpoint = {
   desktop: '1120px',
+  tablet: '768px',
   mobile: '641px',
 } as const;
 
-export const mq = `@media (min-width: ${breakpoint.mobile})`;
+export const mq_lg = `@media (min-width: ${breakpoint.mobile})`;
+export const mq_md = `@media (min-width: ${breakpoint.mobile}) and (max-width: ${breakpoint.desktop})`;
 
 export default breakpoint;
