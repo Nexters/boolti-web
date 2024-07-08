3ec69edf1d525f4f298155cbf432c0c14d50b5ec
Minsu Kim <alstn2468_@naver.com>
2024-07-08 23:08:00 +0900

    feat: 발권 대기 상태 제거

--- packages/api/src/types/common.ts
+++ packages/api/src/types/common.ts
@@ -9,7 +9,7 @@ export type ReservationStatus =
   | 'WAITING_FOR_REFUND'
   | 'REFUND_COMPLETED';
 
-export type TicketStatus = 'WAIT' | 'COMPLETE' | 'CANCEL';
+export type TicketStatus = 'COMPLETE' | 'CANCEL';
 
 export interface PageResponse<T> {
   totalPages: number;
