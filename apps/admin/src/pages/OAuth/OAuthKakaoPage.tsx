e1299031fc25b12d2566e24384035d8ddee7e251
Minsu Kim <alstn2468_@naver.com>
2024-08-05 01:15:44 +0900

    fix: StrictMode 제거 및 불필요한 의존성 제거

--- apps/admin/src/pages/OAuth/OAuthKakaoPage.tsx
+++ apps/admin/src/pages/OAuth/OAuthKakaoPage.tsx
@@ -1,5 +1,5 @@
 import { useKakaoLogin, useKakaoToken, useKakaoUserInfo, useSignUp } from '@boolti/api';
-import { useCallback, useEffect, useMemo } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuthAtom } from '~/atoms/useAuthAtom';
 
@@ -10,45 +10,46 @@ const OAuthKakaoPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const code = useMemo(() => new URLSearchParams(location.search).get('code'), [location.search]);
-
   const kakaoTokenMutation = useKakaoToken();
   const kakaoUserInfoMutation = useKakaoUserInfo();
   const kakaoLoginMutation = useKakaoLogin();
   const signUpMutation = useSignUp();
 
-  const kakaoSignUp = useCallback(
-    async ({ kakaoAccessToken }: { kakaoAccessToken: string }) => {
-      const userInfo = await kakaoUserInfoMutation.mutateAsync({
-        accessToken: kakaoAccessToken,
-        secureResource: true,
-        propertyKeys: [
-          'kakao_account.profile',
-          'kakao_account.name',
-          'kakao_account.email',
-          'kakao_account.age_range',
-          'kakao_account.birthday',
-          'kakao_account.gender',
-        ],
-      });
+  const kakaoSignUp = useCallback(async ({ kakaoAccessToken }: { kakaoAccessToken: string }) => {
+    const userInfo = await kakaoUserInfoMutation.mutateAsync({
+      accessToken: kakaoAccessToken,
+      secureResource: true,
+      propertyKeys: [
+        'kakao_account.profile',
+        'kakao_account.name',
+        'kakao_account.email',
+        'kakao_account.age_range',
+        'kakao_account.birthday',
+        'kakao_account.gender',
+      ],
+    });
 
-      const { accessToken, refreshToken } = await signUpMutation.mutateAsync({
-        nickname: userInfo.kakao_account?.profile?.nickname,
-        email: userInfo.kakao_account?.email,
-        phoneNumber: userInfo.kakao_account?.phone_number,
-        oauthType: 'KAKAO',
-        oauthIdentity: `${userInfo.id}`,
-        imgPath: userInfo.kakao_account?.profile?.profile_image_url,
-      });
+    const { accessToken, refreshToken } = await signUpMutation.mutateAsync({
+      nickname: userInfo.kakao_account?.profile?.nickname,
+      email: userInfo.kakao_account?.email,
+      phoneNumber: userInfo.kakao_account?.phone_number,
+      oauthType: 'KAKAO',
+      oauthIdentity: `${userInfo.id}`,
+      imgPath: userInfo.kakao_account?.profile?.profile_image_url,
+    });
 
-      setToken(accessToken, refreshToken);
-      navigate(PATH.SIGNUP_COMPLETE, { replace: true });
-    },
-    [kakaoUserInfoMutation, navigate, signUpMutation, setToken],
-  );
+    setToken(accessToken, refreshToken);
+    navigate(PATH.SIGNUP_COMPLETE, { replace: true });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const kakaoLogin = useCallback(async () => {
-    if (!code) return;
+    const code = new URLSearchParams(location.search).get('code');
+
+    if (!code) {
+      navigate(PATH.HOME, { replace: true });
+      return;
+    }
 
     const { access_token: kakaoAccessToken } = await kakaoTokenMutation.mutateAsync({ code });
     const { accessToken, refreshToken, signUpRequired } = await kakaoLoginMutation.mutateAsync({
@@ -62,8 +63,9 @@ const OAuthKakaoPage = () => {
     }
 
     setToken(accessToken, refreshToken);
-    navigate(PATH.HOME);
-  }, [code, kakaoLoginMutation, kakaoSignUp, kakaoTokenMutation, navigate, setToken]);
+    navigate(PATH.HOME, { replace: true });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     kakaoLogin();
