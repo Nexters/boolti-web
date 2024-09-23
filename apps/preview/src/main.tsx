153c035b06cccffadaf0473a2662b7eff04dcbf3
Minsu Kim <alstn2468_@naver.com>
2024-09-24 00:40:05 +0900

    fix: 라우터 동작 방식 수정

--- apps/preview/src/main.tsx
+++ apps/preview/src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
