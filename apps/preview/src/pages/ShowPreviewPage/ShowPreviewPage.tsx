7876755d8687d9d9a53e75a7c5307b081b45ca98
Minsu Kim <alstn2468_@naver.com>
2024-09-23 22:37:38 +0900

    fix: name -> property 변경 및 HelmetProvider를 더 상위로 이동

--- apps/preview/src/pages/ShowPreviewPage/ShowPreviewPage.tsx
+++ apps/preview/src/pages/ShowPreviewPage/ShowPreviewPage.tsx
@@ -123,24 +123,30 @@ const ShowPreviewPage = () => {
   return (
     <>
       <Helmet>
-        <meta name="description" content="지금 불티에서 핫한 공연 정보를 확인해 보세요." />
-        <meta property="og:title" content={title} data-react-helmet="true" />
-        <meta property="og:type" content="website" />
         <link rel="shortcut icon" href="https://preview.boolti.in/favicon.png" />
+
+        <title>{title}</title>
+        <link rel="canonical" href="https://preview.boolti.in" />
+        <meta name="description" content="지금 불티에서 핫한 공연 정보를 확인해 보세요." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} data-react-helmet="true" />
         <meta property="og:site_name" content="손쉬운 예매 빠른 입장은 불티" />
-        <meta name="og:description" content="지금 불티에서 핫한 공연 정보를 확인해 보세요." />
-        <meta property="og:url" content="https://boolti.in/" />
+        <meta property="og:description" content="지금 불티에서 핫한 공연 정보를 확인해 보세요." />
+        <meta property="og:url" content={`https://preview.boolti.in/show/${params.showId}`} />
         <meta property="og:image" content="https://preview.boolti.in/thumbnail.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="600" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="boolti.in" />
-        <meta property="twitter:url" content="https://boolti.in/" />
-        <meta name="twitter:title" content={title} data-react-helmet="true" />
-        <meta name="twitter:description" content="지금 불티에서 핫한 공연 정보를 확인해 보세요." />
-        <meta name="twitter:image" content="/thumbnail.png" />
-        <link rel="canonical" href="https://boolti.in/" />
-        <title>{title}</title>
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="preview.boolti.in" />
+        <meta property="twitter:url" content={`https://preview.boolti.in/show/${params.showId}`} />
+        <meta property="twitter:title" content={title} data-react-helmet="true" />
+        <meta
+          property="twitter:description"
+          content="지금 불티에서 핫한 공연 정보를 확인해 보세요."
+        />
+        <meta property="twitter:image" content="/thumbnail.png" />
       </Helmet>
       <Styled.ShowPreviewPage>
         <Styled.ShowPreviewContainer>
