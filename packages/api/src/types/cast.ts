4145bb95bcece4594b92a8e7526ed233bdb42d87
Minsu Kim <alstn2468_@naver.com>
2024-10-05 14:36:03 +0900

    feat: 팀 조회 관련 타입 추가

---
+++ packages/api/src/types/cast.ts
@@ -0,0 +1,33 @@
+export interface ShowCastTeamReadResponse {
+  /** 출연진 팀 식별자 */
+  id: number;
+  /** 출연진 팀 이름 */
+  name: string;
+  /** 출연진 팀 멤버 목록 */
+  members?: Array<{
+    /** 공연 출연진 팀원 ID */
+    id: number;
+    /** 유저 식별 코드 */
+    userCode: string;
+    /** 역할 이름 (1~100자. 빈 문자열 불가) */
+    roleName: string;
+  }>;
+  /** 팀 생성 일시 */
+  createdAt: string;
+  /** 팀 수정 일시 */
+  modifiedAt: string;
+}
+
+export interface ShowCastTeamUpdateRequest {
+  /** 팀 이름 */
+  name: ShowCastTeamReadResponse['name'];
+  /** 팀원 목록 */
+  members: ShowCastTeamReadResponse['members'];
+}
+
+export interface ShowCastTeamCreateRequest {
+  /** 팀 이름 */
+  name: ShowCastTeamReadResponse['name'];
+  /** 팀원 목록 */
+  members: ShowCastTeamReadResponse['members'];
+}
