22fc0d7ceebda87589b676937dc09dec52b315a7
Minsu Kim <alstn2468_@naver.com>
2024-12-12 01:18:37 +0900

    feat: 로깅 개선

--- packages/bridge/src/commands/sendCommand.ts
+++ packages/bridge/src/commands/sendCommand.ts
@@ -5,6 +5,8 @@ const execute = <T>(callback: () => T) => {
   return new Promise((resolve) => resolve(callback()));
 };
 
+const stringify = (object: unknown) => JSON.stringify(object, undefined, 2);
+
 export const sendCommand = async <RequestData = undefined, ResponseData = undefined>(
   command: Command<RequestData>,
 ) =>
@@ -12,8 +14,14 @@ export const sendCommand = async <RequestData = undefined, ResponseData = undefi
     const userAgent = getUserAgent();
     const os = getWebViewOS(userAgent);
 
+    if (!os) {
+      console.warn(`[sendCommand.ts]: not webview, ${stringify(command)}`);
+
+      return command;
+    }
+
     try {
-      console.log(`[sendCommand.ts]: send to ${os}, ${JSON.stringify(command)}`);
+      console.log(`[sendCommand.ts]: send to ${os}, ${stringify(command)}`);
 
       if (checkIsIOS(userAgent) && window.webkit?.messageHandlers?.boolti.postMessage) {
         const result = await window.webkit.messageHandlers.boolti.postMessage<
@@ -21,22 +29,20 @@ export const sendCommand = async <RequestData = undefined, ResponseData = undefi
           ResponseData
         >(command);
 
-        console.log(`[sendCommand.ts]: recive from ios, ${JSON.stringify(result)}`);
+        console.log(`[sendCommand.ts]: recive from ios, ${stringify(result)}`);
 
         return result;
       } else if (checkIsAndroid(userAgent) && window.boolti?.postMessage) {
-        const result = await window.boolti?.postMessage<ResponseData>(JSON.stringify(command));
+        const result = await window.boolti?.postMessage<ResponseData>(stringify(command));
 
-        console.log(`[sendCommand.ts]: recive from aos, ${JSON.stringify(result)}`);
+        console.log(`[sendCommand.ts]: recive from aos, ${stringify(result)}`);
 
         return result;
       }
     } catch (error) {
       if (error instanceof Error) {
         const { name, message } = error;
-        console.log(
-          `[sendCommand.ts] catch error from ${os}, ${JSON.stringify({ name, message })}`,
-        );
+        console.log(`[sendCommand.ts] catch error from ${os}, ${stringify({ name, message })}`);
       } else {
         console.log(`[sendCommand.ts] catch error from ${os}, not error instance`);
       }
