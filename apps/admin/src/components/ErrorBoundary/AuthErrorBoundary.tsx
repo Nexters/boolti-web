ac8813395513591d53df85c8919d784e587dba13
Minsu Kim <alstn2468_@naver.com>
2024-12-16 00:57:38 +0900

    fix: 린트 경고 수정

--- apps/admin/src/components/ErrorBoundary/AuthErrorBoundary.tsx
+++ apps/admin/src/components/ErrorBoundary/AuthErrorBoundary.tsx
@@ -50,6 +50,7 @@ const AuthErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
     };
 
     reset();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return null;
