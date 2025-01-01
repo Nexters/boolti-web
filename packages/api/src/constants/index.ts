541e7660b6a09efa9eb9af1ee19f3de1be67396e
Minsu Kim <alstn2468_@naver.com>
2025-01-01 13:36:43 +0900

    feat: 토큰 갱신 여러번 호출 시도 하는 현상 개선

--- packages/api/src/constants/index.ts
+++ packages/api/src/constants/index.ts
@@ -2,3 +2,6 @@ import { ERROR_CODE } from './errorCode';
 import { LOCAL_STORAGE } from './storages';
 
 export { ERROR_CODE, LOCAL_STORAGE };
+
+export const API_URL = import.meta.env.VITE_BASE_API_URL;
+export const IS_SUPER_ADMIN = import.meta.env.VITE_IS_SUPER_ADMIN === 'true';
--- packages/api/src/fetcher.ts
+++ packages/api/src/fetcher.ts
@@ -1,37 +1,16 @@
 import type { Options, ResponsePromise } from 'ky';
 import ky, { HTTPError } from 'ky';
 
-import { LOCAL_STORAGE } from './constants';
-import { checkIsWebView, isWebViewBridgeAvailable, requestToken } from '@boolti/bridge';
-
-const API_URL = import.meta.env.VITE_BASE_API_URL;
-const IS_SUPER_ADMIN = import.meta.env.VITE_IS_SUPER_ADMIN === 'true';
-
-interface PostRefreshTokenResponse {
-  accessToken: string;
-  refreshToken: string;
-}
-
-const postRefreshToken = async () => {
-  const refreshToken = window.localStorage.getItem(LOCAL_STORAGE.REFRESH_TOKEN);
-
-  if (refreshToken) {
-    const response = await ky.post(
-      `${API_URL}/${IS_SUPER_ADMIN ? 'sa-api' : 'web'}/papi/v1/login/refresh`,
-      {
-        json: {
-          refreshToken,
-        },
-      },
-    );
-    return await response.json<PostRefreshTokenResponse>();
-  }
-};
+import { API_URL, LOCAL_STORAGE } from './constants';
+import { Mutex } from 'async-mutex';
+import { refreshAccessToken } from './refreshAccessToken';
 
 const defaultOption: Options = {
   retry: 0,
   timeout: 30_000,
 };
+
+const tokenRefreshMutex = new Mutex();
 
 export const instance = ky.create({
   prefixUrl: API_URL,
@@ -50,29 +29,16 @@ export const instance = ky.create({
     ],
     afterResponse: [
       async (request, options, response) => {
-        // access token이 만료되었을 때, refresh token으로 새로운 access token을 발급받는다.
         if (!response.ok && response.status === 401 && !request.url.includes('logout')) {
-          let newAccessToken: string | undefined = undefined,
-            newRefreshToken: string | undefined = undefined;
+          if (tokenRefreshMutex.isLocked()) {
+            await tokenRefreshMutex.waitForUnlock();
+          }
           try {
-            if (checkIsWebView() && isWebViewBridgeAvailable()) {
-              newAccessToken = (await requestToken()).data.token;
-            } else {
-              const { accessToken, refreshToken } = (await postRefreshToken()) ?? {};
-              newAccessToken = accessToken;
-              newRefreshToken = refreshToken;
-            }
+            tokenRefreshMutex.acquire();
 
-            if (newAccessToken) {
-              window.localStorage.setItem(LOCAL_STORAGE.ACCESS_TOKEN, newAccessToken);
-
-              if (newRefreshToken) {
-                window.localStorage.setItem(LOCAL_STORAGE.REFRESH_TOKEN, newRefreshToken);
-              }
-
-              request.headers.set('Authorization', `Bearer ${newAccessToken}`);
-              return ky(request, options);
-            }
+            const newAccessToken = await refreshAccessToken();
+            request.headers.set('Authorization', `Bearer ${newAccessToken}`);
+            return ky(request, options);
           } catch (e) {
             if (e instanceof HTTPError && e.response.url.includes('/login/refresh')) {
               window.localStorage.removeItem(LOCAL_STORAGE.ACCESS_TOKEN);
@@ -82,6 +48,8 @@ export const instance = ky.create({
             if (e instanceof Error) {
               console.warn(`[fether.ts] ${e.name} (${e.message})`);
             }
+          } finally {
+            tokenRefreshMutex.release();
           }
         }
         return response;
