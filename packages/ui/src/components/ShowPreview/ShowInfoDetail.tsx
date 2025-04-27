92cadae5c14b717cacd8e5cd5c88a3df22fe402a
Shim MunSeong <Puterism.K@gmail.com>
2025-04-27 20:50:00 +0900

    fix: 디자인 QA 반영
    - 공연 상세 미리보기 타이틀 글자색 반영
    - 내용과 위치 섹션 서로 위치 변경
    - 복사 버튼 위치 수정

--- packages/ui/src/components/ShowPreview/ShowInfoDetail.tsx
+++ packages/ui/src/components/ShowPreview/ShowInfoDetail.tsx
@@ -82,39 +82,6 @@ const ShowInfoDetail = ({
           {salesStartTime} - {salesEndTime}
         </Styled.ShowInfoDescription>
       </Styled.ShowInfoGroup>
-      <Styled.ShowInfoGroup>
-        <Styled.ShowInfoTitleContainer>
-          <Styled.ShowInfoTitle>위치</Styled.ShowInfoTitle>
-        </Styled.ShowInfoTitleContainer>
-        <Styled.ShowInfoSubtitle>{placeName}</Styled.ShowInfoSubtitle>
-        <Styled.ShowInfoDescription>
-          {`${streetAddress} / ${detailAddress} ・ `}
-          <Styled.ShowInfoTitleButton
-            type="button"
-            onClick={() => {
-              navigator.clipboard.writeText(`${streetAddress} ${detailAddress}`);
-              if (checkIsWebView()) {
-                showToast({ message: '주소를 복사했어요.', duration: TOAST_DURATIONS.SHORT });
-              } else {
-                alert('공연장 주소가 복사되었어요');
-              }
-            }}
-          >
-            복사
-          </Styled.ShowInfoTitleButton>
-        </Styled.ShowInfoDescription>
-        {latitude && longitude && (
-          <PreviewMap latitude={latitude} longitude={longitude} name={placeName} />
-        )}
-        {isEnded && soldTicketCount !== undefined && (
-          <Styled.ShowTicketInfoDescription>
-            <Styled.TicketIcon>
-              <TicketIcon />
-            </Styled.TicketIcon>{' '}
-            {soldTicketCount}매 판매 완료
-          </Styled.ShowTicketInfoDescription>
-        )}
-      </Styled.ShowInfoGroup>
       <Styled.ShowInfoGroup style={{ paddingBottom: 0 }}>
         <Styled.ShowInfoTitleContainer>
           <Styled.ShowInfoTitle>내용</Styled.ShowInfoTitle>
@@ -129,6 +96,41 @@ const ShowInfoDetail = ({
           }}>
             {collapse ? <>내용 더 보기 <ChevronDownIcon /></> : <>내용 접기 <ChevronUpIcon /></>}
           </Styled.ShowInfoMoreButton>
+        )}
+      </Styled.ShowInfoGroup>
+      <Styled.ShowInfoGroup>
+        <Styled.ShowInfoTitleContainer>
+          <Styled.ShowInfoTitle>위치</Styled.ShowInfoTitle>
+        </Styled.ShowInfoTitleContainer>
+        <Styled.ShowInfoSubtitle>{placeName}</Styled.ShowInfoSubtitle>
+        <Styled.ShowInfoDescription>
+          <Styled.ShowInfoDescriptionText>
+            {`${streetAddress} / ${detailAddress} ・ `}
+            <Styled.ShowInfoDescriptionButton
+              type="button"
+              onClick={() => {
+                navigator.clipboard.writeText(`${streetAddress} ${detailAddress}`);
+                if (checkIsWebView()) {
+                  showToast({ message: '주소를 복사했어요.', duration: TOAST_DURATIONS.SHORT });
+                } else {
+                  alert('공연장 주소가 복사되었어요');
+                }
+              }}
+            >
+              복사
+            </Styled.ShowInfoDescriptionButton>
+          </Styled.ShowInfoDescriptionText>
+        </Styled.ShowInfoDescription>
+        {latitude && longitude && (
+          <PreviewMap latitude={latitude} longitude={longitude} name={placeName} />
+        )}
+        {isEnded && soldTicketCount !== undefined && (
+          <Styled.ShowTicketInfoDescription>
+            <Styled.TicketIcon>
+              <TicketIcon />
+            </Styled.TicketIcon>{' '}
+            {soldTicketCount}매 판매 완료
+          </Styled.ShowTicketInfoDescription>
         )}
       </Styled.ShowInfoGroup>
       <Styled.ShowInfoGroup>
