fbfd3b24a5b99b56a5c102727e1ffe8aae30c8a9
Shim MunSeong <Puterism.K@gmail.com>
2025-05-04 23:22:08 +0900

    feat: 공연 미리보기 데스크탑 버전에서 공유 드롭다운 구현

--- packages/ui/src/components/ShowPreview/index.tsx
+++ packages/ui/src/components/ShowPreview/index.tsx
@@ -1,7 +1,7 @@
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { BooltiDark, ClockMobileIcon, MapMarkerIcon, ShareIcon } from '@boolti/icon';
@@ -38,26 +38,52 @@ interface ShowPreviewProps {
       userImgPath: string;
     }[];
   }>;
+  shareDropdownOpen?: boolean;
   logoLinkHref?: string;
   containerRef?: React.RefObject<HTMLDivElement>;
   onClickLink?: () => void;
   onClickLinkMobile?: () => void;
   onClickShareButton?: () => void;
+  onShareShowPreviewLink?: () => void;
+  onShareShowInfo?: () => void;
+  onCloseShareDropdown?: () => void;
 }
 
 const ShowPreview = ({
   show,
   showCastTeams,
+  shareDropdownOpen,
   logoLinkHref,
   containerRef,
   onClickLink,
   onClickLinkMobile,
   onClickShareButton,
+  onShareShowPreviewLink,
+  onShareShowInfo,
+  onCloseShareDropdown,
 }: ShowPreviewProps) => {
   const { images, name, date, startTime, runningTime, placeName } = show;
 
   const [noticeOpen, setNoticeOpen] = useState<boolean>(false);
   const containerScrollTop = useRef<number | null>(null);
+  const shareDropdownMenuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const outsideClickHandler = (event: MouseEvent) => {
+      if (
+        shareDropdownOpen &&
+        !shareDropdownMenuRef.current?.contains(event.target as HTMLElement)
+      ) {
+        onCloseShareDropdown?.();
+      }
+    };
+
+    document.addEventListener('click', outsideClickHandler);
+
+    return () => {
+      document.removeEventListener('click', outsideClickHandler);
+    };
+  }, [onCloseShareDropdown, shareDropdownOpen]);
 
   if (noticeOpen) {
     return (
@@ -81,9 +107,29 @@ const ShowPreview = ({
         <Styled.LogoLink href={logoLinkHref}>
           <BooltiDark />
         </Styled.LogoLink>
-        <Styled.ShareButton onClick={onClickShareButton} disabled={!onClickShareButton}>
+        <Styled.ShareButton
+          onClick={(event) => {
+            event.stopPropagation();
+            onClickShareButton?.();
+          }}
+          disabled={!onClickShareButton}
+        >
           <ShareIcon />
         </Styled.ShareButton>
+        <Styled.ShareDropdownMenu ref={shareDropdownMenuRef} open={!!shareDropdownOpen}>
+          <Styled.ShareDropdownItem
+            onClick={() => {
+              onShareShowPreviewLink?.();
+            }}
+          >
+            URL만 공유하기
+          </Styled.ShareDropdownItem>
+          <Styled.ShareDropdownItem
+            onClick={() => {
+              onShareShowInfo?.();
+            }}
+          >공연 정보 함께 공유하기</Styled.ShareDropdownItem>
+        </Styled.ShareDropdownMenu>
       </Styled.ShowPreviewNavbar>
       <Styled.ShowPreviewHeader>
         <Swiper
