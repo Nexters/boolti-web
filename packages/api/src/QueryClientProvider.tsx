53897f1b1d22e295d304b992e3450e4fbe12a404
jiji-hoon96 <jihoon7705@gmail.com>
2025-12-07 16:51:35 +0900

    fix: profile qa 3차 반영
    - queryclient 롤백

--- packages/api/src/QueryClientProvider.tsx
+++ packages/api/src/QueryClientProvider.tsx
@@ -1,7 +1,20 @@
 import { QueryClient, QueryClientProvider as BaseQueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export function QueryClientProvider({ children }: React.PropsWithChildren) {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: false,
+            staleTime: 5000,
+            useErrorBoundary: true,
+          },
+        },
+      }),
+  );
 
   return <BaseQueryClientProvider client={queryClient}>{children}</BaseQueryClientProvider>;
 }
