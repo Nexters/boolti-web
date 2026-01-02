5f68901453b35cca32b83834a279233faee8c868
JIHOON LEE <jihoon.lee@hicare.net>
2026-01-02 17:48:21 +0900

    test: 동적 이미지 테스트

--- apps/profile/src/components/Meta/index.tsx
+++ apps/profile/src/components/Meta/index.tsx
@@ -11,6 +11,10 @@ export const Meta = ({ nickname, introduction, imgPath, userCode }: Props) => {
   const title = `#{${nickname}}`;
   const description = introduction || '핫한 공연 예매의 시작, 불티';
   const profileUrl = `https://profile.boolti.in/${userCode}`;
+  const ogImage =
+    userCode && userCode.length > 0
+      ? `https://api.boolti.in/web/papi/v2/users/${userCode}/profile-image`
+      : imgPath;
 
   return (
     <Helmet>
@@ -25,9 +29,9 @@ export const Meta = ({ nickname, introduction, imgPath, userCode }: Props) => {
       <meta property="og:site_name" content="불티" />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={profileUrl} />
-      {imgPath && (
+      {ogImage && (
         <>
-          <meta property="og:image" content={imgPath} />
+          <meta property="og:image" content={ogImage} />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="600" />
         </>
@@ -38,7 +42,7 @@ export const Meta = ({ nickname, introduction, imgPath, userCode }: Props) => {
       <meta property="twitter:url" content={profileUrl} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
-      {imgPath && <meta property="twitter:image" content={imgPath} />}
+      {ogImage && <meta property="twitter:image" content={ogImage} />}
     </Helmet>
   );
 };
