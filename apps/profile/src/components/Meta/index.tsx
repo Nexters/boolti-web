fa3cf61121ca63adcbeb75b6672f7a9e10437245
jiji-hoon96 <jihoon7705@gmail.com>
2026-01-04 16:59:30 +0900

    feat: profile og 출력 방식 변경

--- apps/profile/src/components/Meta/index.tsx
+++ apps/profile/src/components/Meta/index.tsx
@@ -3,18 +3,17 @@ import { Helmet } from 'react-helmet-async';
 interface Props {
   nickname: string;
   introduction: string;
-  imgPath: string;
   userCode: string;
 }
 
-export const Meta = ({ nickname, introduction, imgPath, userCode }: Props) => {
+export const Meta = ({ nickname, introduction, userCode }: Props) => {
   const title = `#{${nickname}}`;
   const description = introduction || '핫한 공연 예매의 시작, 불티';
   const profileUrl = `https://profile.boolti.in/${userCode}`;
-  const ogImage =
-    userCode && userCode.length > 0
-      ? `https://api.boolti.in/web/papi/v2/users/${userCode}/profile-image`
-      : imgPath;
+
+  const ogTitle = '핫한 공연 예매의 시작, 불티';
+  const ogDescription = '지금 불티에서 핫한 아티스트 정보를 확인해 보세요.';
+  const ogImage = 'https://profile.boolti.in/profile_og_thumbnail.png';
 
   return (
     <Helmet>
@@ -25,24 +24,20 @@ export const Meta = ({ nickname, introduction, imgPath, userCode }: Props) => {
       <meta name="description" content={description} />
 
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={title} />
+      <meta property="og:title" content={ogTitle} />
       <meta property="og:site_name" content="불티" />
-      <meta property="og:description" content={description} />
+      <meta property="og:description" content={ogDescription} />
       <meta property="og:url" content={profileUrl} />
-      {ogImage && (
-        <>
-          <meta property="og:image" content={ogImage} />
-          <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="600" />
-        </>
-      )}
+      <meta property="og:image" content={ogImage} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="600" />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="profile.boolti.in" />
       <meta property="twitter:url" content={profileUrl} />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
-      {ogImage && <meta property="twitter:image" content={ogImage} />}
+      <meta property="twitter:title" content={ogTitle} />
+      <meta property="twitter:description" content={ogDescription} />
+      <meta property="twitter:image" content={ogImage} />
     </Helmet>
   );
 };
