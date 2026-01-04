00b43a795ee6c4d6d3c12718b629ad452ae1b221
jiji-hoon96 <jihoon7705@gmail.com>
2026-01-04 17:30:36 +0900

    fix: 동적 경로 추가

--- apps/profile/src/components/Meta/index.tsx
+++ apps/profile/src/components/Meta/index.tsx
@@ -11,10 +11,6 @@ export const Meta = ({ nickname, introduction, userCode }: Props) => {
   const description = introduction || '핫한 공연 예매의 시작, 불티';
   const profileUrl = `https://profile.boolti.in/${userCode}`;
 
-  const ogTitle = '핫한 공연 예매의 시작, 불티';
-  const ogDescription = '지금 불티에서 핫한 아티스트 정보를 확인해 보세요.';
-  const ogImage = 'https://profile.boolti.in/profile_og_thumbnail.png';
-
   return (
     <Helmet>
       <link rel="shortcut icon" href="https://profile.boolti.in/favicon.png" />
@@ -22,15 +18,6 @@ export const Meta = ({ nickname, introduction, userCode }: Props) => {
       <title>{title}</title>
       <link rel="canonical" href={profileUrl} />
       <meta name="description" content={description} />
-
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={ogTitle} />
-      <meta property="og:site_name" content="불티" />
-      <meta property="og:description" content={ogDescription} />
-      <meta property="og:url" content={profileUrl} />
-      <meta property="og:image" content={ogImage} />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="600" />
     </Helmet>
   );
 };
