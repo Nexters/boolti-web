1c0a1bb27d9be2b721ead1e7e436b346cf0666d2
JIHOON LEE <jihoon.lee@hicare.net>
2026-03-25 23:31:35 +0900

    fix(super-admin): super admin 조건 추가

--- packages/api/src/queries/useHostList.ts
+++ packages/api/src/queries/useHostList.ts
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 
 import { queryKeys } from '../queryKey';
 
-const useHostList = (showId: number) => useQuery(queryKeys.host.list(showId));
+const useHostList = (showId: number, options?: { enabled?: boolean }) =>
+  useQuery({ ...queryKeys.host.list(showId), ...options });
 
 export default useHostList;
